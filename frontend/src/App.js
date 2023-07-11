452b2fa8898f3273ae3e78c0f30a95ebcda8056e
the-null-character <akshay.2020a@vitstudent.ac.in>
2023-07-11 12:22:27 +0530

    all the damn files

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import Header from "./Components/header";
+import AddStudent from "./Components/AddStudent";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ShowStudents from "./Components/ShowStudent";
+import ShowOne from "./Components/ShowOne";
+import Update from "./Components/Update";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Router>
+        <Routes>
+          <Route path="/get/:id" exact Component={Update} />
+          <Route path="/getMarks" exact Component={ShowOne} />
+          <Route path="/add" exact Component={AddStudent} />
+          <Route path="/get" exact Component={ShowStudents} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
